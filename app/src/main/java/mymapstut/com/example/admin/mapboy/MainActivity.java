2458eb9bbc76e920b7b93254580b36dd78b2a4fc
audiotrash112@gmail.com <audiotrash112@gmail.com>
2017-05-27 17:49:48 -0400

    Initial commit

---
+++ app/src/main/java/mymapstut/com/example/admin/mapboy/MainActivity.java
@@ -0,0 +1,26 @@
+package mymapstut.com.example.admin.mapboy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
